45a19429b6447c8992079de8e919bcb627470efd
Lawrence Mulligan <lmulligan@lpmnet.com>
2017-02-11 10:51:09 -0600

    Added lpm variables

--- M4/Deploy-UnicornerPaas.ps1
+++ M4/Deploy-UnicornerPaas.ps1
@@ -1,9 +1,9 @@
 ﻿### Define variables
 {
-$resourceGroupLocation = 'Australia Southeast'
-$resourceGroupName = 'unicorner-paas'
-$resourceDeploymentName = 'unicorner-paas-deployment'
-$templatePath = $env:SystemDrive + '\pluralsight'
+$resourceGroupLocation = 'North Central US'
+$resourceGroupName = 'lpm-unicorner-paas'
+$resourceDeploymentName = 'lpm-unicorner-paas-deployment'
+$templatePath = $env:USERPROFILE + '\Development\Azure\azure-arm-dd\M4'
 $templateFile = 'unicornerPaas.json'
 $template = $templatePath + '\' + $templateFile
 }
